972d7cee6279bfb0266c68552765cc07b0f11e8e
turku <turkucakmak@hotmail.com>
2023-05-31 01:52:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event){
+    let newarr = [];
+    for (let i=0; i< array.length; i++){
+        newarr.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+
+    }
+return newarr;
+}
+
+function countDown(integer){
+    while(integer>=0){
+        console.log(integer);
+        integer--;
+    }
+}
